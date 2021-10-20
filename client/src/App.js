1beab969533ac32be4060497b3b129998d7fb5a9
IchikonWD <ezequielmigueles@icloud.com>
2021-10-20 16:50:39 +0200

    feat/BasicRouting

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
-import Main from '../src/Components/Main'
+import Main from '../src/Components/Main';
+import Header from '../src/Components/Header';
+import Footer from '../src/Components/Footer';
 
 function App() {
-  return <div className='App'>
-    <Main/>
-    </div>;
+  return (
+    <div className='App'>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- client/src/Components/Main/Main.jsx
+++ client/src/Components/Main/Main.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Home from '../Home';
 import Register from '../Register';
@@ -10,10 +10,12 @@ const Main = () => {
   return (
     <main>
       <Router>
-        <Route path='/' component={Home} exact />
-        <Route path='/register' component={Register} />
-        <Route path='/login' component={Login} />
-        <Route path='/error' component={Error} />
+        <Switch>
+          <Route path='/' component={Home} exact />
+          <Route path='/register' component={Register} />
+          <Route path='/login' component={Login} />
+          <Route component={Error} />
+        </Switch>
       </Router>
     </main>
   );
