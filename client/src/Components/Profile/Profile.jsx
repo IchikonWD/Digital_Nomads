334977452868aa5f9bdd8e630f6d46d20c96a720
Fabrizio <fabacasa@gmail.com>
2021-10-21 00:11:57 +0200

    feat/profile initialized

--- client/src/Components/Profile/Profile.jsx
+++ client/src/Components/Profile/Profile.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <div className="user_profile">
+      <img src="#" alt="user_nickname" />
+      <h5>Nickname:</h5>
+      <span></span>
+      <h5>Email:</h5>
+      <span></span>
+      <h5>Name:</h5>
+      <span></span>
+      <h5>Last Name:</h5>
+      <span></span>
+      <h5>Age:</h5>
+      <span></span>
+      <h5>Language:</h5>
+      <span></span>
+      <h5>Ocupation:</h5>
+      <span></span>
+      <h5>Country:</h5>
+      <span></span>
+      <button>Change interests</button>
+    </div>
+  )
 };
 
 export default Profile;
