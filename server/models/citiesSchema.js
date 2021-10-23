93ef576943344357a67a3553bd9aa432f0f260f4
IchikonWD <ezequielmigueles@icloud.com>
2021-10-23 18:38:05 +0200

    Hotfix

--- server/models/citiesSchema.js
+++ server/models/citiesSchema.js
@@ -1,5 +1,9 @@
 import { pool, client } from '../config/sqldb.js';
 
+// @desc    Get All Cities
+// @route   Get /api/data/
+// @access  Public
+
 const getAllCities = async () => {
   let connection, response;
   try {
@@ -14,14 +18,27 @@ const getAllCities = async () => {
   }
   return response.rows;
 };
+
+// @desc    Get One City
+// @route   Get /api/data/:city
+// @access  Public
+
 const getOneCity = async (param) => {
-  city = param.charAt(0).toUpperCase() + param.slice(1);
+  // Camelcase param if has two words or more
+  if (param.includes('_')) {
+    param = param.replace(/_/g, ' ');
+    param = param.replace(/\w\S*/g, function (txt) {
+      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+  } else {
+    param = param.charAt(0).toUpperCase() + param.substr(1).toLowerCase();
+  }
   let connection, response;
   try {
     connection = await pool.connect();
     let sql_query =
       'SELECT * FROM cities WHERE id=(SELECT id FROM cities WHERE name=$1)';
-    response = await pool.query(sql_query, [city]);
+    response = await pool.query(sql_query, [param]);
     console.log(response);
   } catch (err) {
     console.log(err.stack);
