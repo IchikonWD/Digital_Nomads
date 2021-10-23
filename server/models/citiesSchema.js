72f80fb99155fdf3bb9f09dd8fe7cf9cb1a5165a
IchikonWD <ezequielmigueles@icloud.com>
2021-10-23 18:30:54 +0200

    hotfix

--- server/models/citiesSchema.js
+++ server/models/citiesSchema.js
@@ -1,34 +1,34 @@
-import { pool, client } from '../config/sqldb.js'
+import { pool, client } from '../config/sqldb.js';
 
 const getAllCities = async () => {
-    let connection, response
-    try {
-        connection = await pool.connect()
-        let sql_query = 'SELECT * FROM cities'
-        response = await pool.query(sql_query)
-        console.log(response);
-    } catch (err) {
-        console.log(err.stack)
-    }
-    finally {
-        connection.release()
-    }
-    return response.rows
-}
+  let connection, response;
+  try {
+    connection = await pool.connect();
+    let sql_query = 'SELECT * FROM cities';
+    response = await pool.query(sql_query);
+    console.log(response);
+  } catch (err) {
+    console.log(err.stack);
+  } finally {
+    connection.release();
+  }
+  return response.rows;
+};
 const getOneCity = async (param) => {
-    let connection, response
-    try {
-        connection = await pool.connect()
-        let sql_query = 'SELECT * FROM cities WHERE id=(SELECT id FROM cities WHERE name=$1)'
-        response = await pool.query(sql_query, [param])
-        console.log(response);
-    } catch (err) {
-        console.log(err.stack)
-    }
-    finally {
-        connection.release()
-    }
-    return response.rows
-}
+  city = param.charAt(0).toUpperCase() + param.slice(1);
+  let connection, response;
+  try {
+    connection = await pool.connect();
+    let sql_query =
+      'SELECT * FROM cities WHERE id=(SELECT id FROM cities WHERE name=$1)';
+    response = await pool.query(sql_query, [city]);
+    console.log(response);
+  } catch (err) {
+    console.log(err.stack);
+  } finally {
+    connection.release();
+  }
+  return response.rows;
+};
 
-export { getAllCities, getOneCity }
+export { getAllCities, getOneCity };
