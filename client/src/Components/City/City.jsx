e6e1b4c73bf191134337629e981508437041189a
IchikonWD <ezequielmigueles@icloud.com>
2021-10-27 16:25:36 +0200

    /featReviewSysFinal completed

--- client/src/Components/City/City.jsx
+++ client/src/Components/City/City.jsx
@@ -21,6 +21,13 @@ const City = ({ history, match }) => {
   const [reviewing, setReviewing] = useState(false);
   const [combinedReview, setCombinedReview] = useState([]);
 
+  // Send Review States
+  const [completeReview, setCompleteReview] = useState({});
+  const [internetReview, setInternetReview] = useState(0);
+  const [safetyReview, setSafetyReview] = useState(0);
+  const [coworkingReview, setCoworkingReview] = useState({});
+  const [comment, setComment] = useState('');
+
   useEffect(() => {
     const nycCost = 1366.67;
     const cityCost = nycCost * (livingCost / 100);
@@ -118,14 +125,113 @@ const City = ({ history, match }) => {
     }
   }, [reviewersName, Reviews, reviewersAvatar]);
 
+  useEffect(() => {
+    const review = {
+      user: {
+        _id: user._id,
+      },
+      ratings: {
+        internetConnection: internetReview,
+        overallSafety: safetyReview,
+        coworkingSpace: coworkingReview,
+      },
+      comment: comment,
+    };
+    setCompleteReview(review);
+  }, [internetReview, safetyReview, coworkingReview, comment]);
+
   const handleButton = async (e) => {
     e.preventDefault();
     setReviewing(!reviewing);
   };
 
-  const handleSendButton = async (e) => {
+  const handleInternet1 = async (e) => {
     e.preventDefault();
-    setReviewing(!reviewing);
+    setInternetReview(1);
+  };
+
+  const handleInternet2 = async (e) => {
+    e.preventDefault();
+    setInternetReview(2);
+  };
+
+  const handleInternet3 = async (e) => {
+    e.preventDefault();
+    setInternetReview(3);
+  };
+
+  const handleInternet4 = async (e) => {
+    e.preventDefault();
+    setInternetReview(4);
+  };
+
+  const handleInternet5 = async (e) => {
+    e.preventDefault();
+    setInternetReview(5);
+  };
+
+  const handleSafety1 = async (e) => {
+    e.preventDefault();
+    setSafetyReview(1);
+  };
+
+  const handleSafety2 = async (e) => {
+    e.preventDefault();
+    setSafetyReview(2);
+  };
+
+  const handleSafety3 = async (e) => {
+    e.preventDefault();
+    setSafetyReview(3);
+  };
+
+  const handleSafety4 = async (e) => {
+    e.preventDefault();
+    setSafetyReview(4);
+  };
+
+  const handleSafety5 = async (e) => {
+    e.preventDefault();
+    setSafetyReview(5);
+  };
+
+  const handleCoworking = async (e) => {
+    e.preventDefault();
+    setCoworkingReview(false);
+  };
+  const handleCoworking2 = async (e) => {
+    e.preventDefault();
+    setCoworkingReview(true);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (completeReview === '') {
+      alert('Please rate your experience');
+    } else if (internetReview === 0) {
+      alert('Please rate your internet connection');
+    } else if (safetyReview === 0) {
+      alert('Please rate your safety');
+    } else if (comment === '') {
+      alert('Please write a comment');
+    } else {
+      console.log('Submited!');
+      const config = {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
+      try {
+        const url = `/api/cities/${cityName}/reviews`;
+        const result = await axios.post(url, completeReview, config);
+        console.log(result, 'Review enviada correctamente');
+        setReviewing(!reviewing);
+      } catch (error) {
+        console.log(error);
+        setReviewing(!reviewing);
+      }
+    }
   };
 
   if (reviewing === true && user.isLoggedIn === true) {
@@ -138,9 +244,9 @@ const City = ({ history, match }) => {
           <h1 className='review__header__title'>{cityName}</h1>
           <i className='far fa-heart review__header__icon2'></i>
         </header>
-        <form className='review__form'>
+        <form className='review__form' onSubmit={handleSubmit} method='POST'>
           <h2 className='review__form__title1'>
-            Tell your experience and <br />
+            Share your experience and <br />
             help other nomads like you
           </h2>
           <input
@@ -149,32 +255,181 @@ const City = ({ history, match }) => {
             id='comment'
             placeholder='Type to write'
             className='review__form__input1'
+            onChange={(e) => setComment(e.target.value)}
           />
           <h2 className='review__form__title2'>Rate the following items</h2>
           <div className='review__form__container'>
-            <h4>Internet connection</h4>
-            <i className='fas fa-wifi'></i>
-            <i className='fas fa-wifi'></i>
-            <i className='fas fa-wifi'></i>
-            <i className='fas fa-wifi'></i>
-            <i className='fas fa-wifi'></i>
+            <h4 className='review__form__container__title'>
+              Internet connection
+            </h4>
+            <button
+              className='button_review_1 hidden_button'
+              onClick={handleInternet1}
+            >
+              <img
+                src={
+                  internetReview === 1
+                    ? '/assets/icons/internet1.png'
+                    : '/assets/icons/internetFalse.png'
+                }
+                alt='internet'
+              />
+            </button>
+            <button
+              className='button_review_2 hidden_button'
+              onClick={handleInternet2}
+            >
+              <img
+                src={
+                  internetReview === 2
+                    ? '/assets/icons/internet2.png'
+                    : '/assets/icons/internetFalse.png'
+                }
+                alt='internet'
+              />
+            </button>
+            <button
+              className='button_review_3 hidden_button'
+              onClick={handleInternet3}
+            >
+              <img
+                src={
+                  internetReview === 3
+                    ? '/assets/icons/internet3.png'
+                    : '/assets/icons/internetFalse.png'
+                }
+                alt='internet'
+              />
+            </button>
+            <button
+              className='button_review_4 hidden_button'
+              onClick={handleInternet4}
+            >
+              <img
+                src={
+                  internetReview === 4
+                    ? '/assets/icons/internet4.png'
+                    : '/assets/icons/internetFalse.png'
+                }
+                alt='internet'
+              />
+            </button>
+            <button
+              className='button_review_5 hidden_button'
+              onClick={handleInternet5}
+            >
+              <img
+                src={
+                  internetReview === 5
+                    ? '/assets/icons/internet5.png'
+                    : '/assets/icons/internetFalse.png'
+                }
+                alt='internet'
+              />
+            </button>
           </div>
           <div className='review__form__container'>
-            <h4>Overall safety</h4>
-            <i className='fas fa-meh-blank'></i>
-            <i className='fas fa-meh-blank'></i>
-            <i className='fas fa-meh-blank'></i>
-            <i className='fas fa-meh-blank'></i>
-            <i className='fas fa-meh-blank'></i>
+            <h4 className='review__form__container__title'>Overall safety</h4>
+            <button
+              className='button_review_1 hidden_button'
+              onClick={handleSafety1}
+            >
+              <img
+                src={
+                  safetyReview === 1
+                    ? '/assets/icons/safety1.png'
+                    : '/assets/icons/safetyFalse.png'
+                }
+                alt='safety'
+              />
+            </button>
+            <button
+              className='button_review_2 hidden_button'
+              onClick={handleSafety2}
+            >
+              <img
+                src={
+                  safetyReview === 2
+                    ? '/assets/icons/safety2.png'
+                    : '/assets/icons/safetyFalse.png'
+                }
+                alt='safety'
+              />
+            </button>
+            <button
+              className='button_review_3 hidden_button'
+              onClick={handleSafety3}
+            >
+              <img
+                src={
+                  safetyReview === 3
+                    ? '/assets/icons/safety3.png'
+                    : '/assets/icons/safetyFalse.png'
+                }
+                alt='safety'
+              />
+            </button>
+            <button
+              className='button_review_4 hidden_button'
+              onClick={handleSafety4}
+            >
+              <img
+                src={
+                  safetyReview === 4
+                    ? '/assets/icons/safety4.png'
+                    : '/assets/icons/safetyFalse.png'
+                }
+                alt='safety'
+              />
+            </button>
+            <button
+              className='button_review_5 hidden_button'
+              onClick={handleSafety5}
+            >
+              <img
+                src={
+                  safetyReview === 5
+                    ? '/assets/icons/safety5.png'
+                    : '/assets/icons/safetyFalse.png'
+                }
+                alt='safety'
+              />
+            </button>
           </div>
           <div className='review__form__container'>
-            <h4>Coworking spaces near you?</h4>
-            <i className='fas fa-suitcase'></i>
-            <i className='fas fa-suitcase'></i>
+            <h4 className='review__form__container__title'>
+              Coworking spaces near you?
+            </h4>
+            <button
+              className='button_review_true hidden_button'
+              onClick={handleCoworking}
+            >
+              <img
+                src={
+                  coworkingReview === false
+                    ? '/assets/icons/jobs1.png'
+                    : '/assets/icons/jobsFalse.png'
+                }
+                alt='jobs'
+              />
+            </button>
+            <button
+              className='button_review_false hidden_button'
+              onClick={handleCoworking2}
+            >
+              <img
+                src={
+                  coworkingReview === true
+                    ? '/assets/icons/jobs2.png'
+                    : '/assets/icons/jobsFalse.png'
+                }
+                alt='jobs'
+              />
+            </button>
           </div>
           <button
             className='primary_button review__form__button '
-            onClick={handleSendButton}
+            type='submit'
           >
             Send
           </button>
