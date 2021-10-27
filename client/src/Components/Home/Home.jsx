320c164605351ddc2ec27ebfdc18070502118e80
IchikonWD <ezequielmigueles@icloud.com>
2021-10-27 03:45:40 +0200

    hotfix TODO: fix link city names

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -1,49 +1,48 @@
 import React, { useContext, useState, useEffect } from 'react';
-import Slider from 'react-touch-drag-slider'
+import Slider from 'react-touch-drag-slider';
 import { DataContext } from '../../Contexts/dataContext';
-import axios from 'axios'
+import axios from 'axios';
 
 const Home = ({ history }) => {
   const { data, setData } = useContext(DataContext);
-  const [cities, setCities] = useState([])
-  const [text, setText] = useState("")
-  const [suggestions, setSuggestions] = useState([])
+  const [cities, setCities] = useState([]);
+  const [text, setText] = useState('');
+  const [suggestions, setSuggestions] = useState([]);
   useEffect(() => {
     // Get's all city names and loads them for the suggestion section
     const loadCities = async () => {
-      const url = `http://localhost:5000/api/data`
-      const res = await axios.get(url)
-      console.log(res.data);
-      setCities(res.data)
-    }
-    loadCities()
-  }, [])
+      const url = `http://localhost:5000/api/data`;
+      const res = await axios.get(url);
+      setCities(res.data);
+    };
+    loadCities();
+  }, []);
   // Filters all city names and matches it with the input text
   const onChangeHandler = (text) => {
     let modText = text.charAt(0).toUpperCase() + text.substr(1).toLowerCase();
-    let matches = []
+    let matches = [];
     if (text.length > 0) {
       matches = cities.filter((city) => {
-        const regex = new RegExp(`${modText}`)
-        return city.name.match(regex)
-      })
+        const regex = new RegExp(`${modText}`);
+        return city.name.match(regex);
+      });
     }
     console.log(matches);
-    setSuggestions(matches)
-    setText(text)
-  }
+    setSuggestions(matches);
+    setText(text);
+  };
   // Sets the suggestion value to the input value
   const onSuggestHandler = (text) => {
-    setText(text)
-    setSuggestions([])
-  }
+    setText(text);
+    setSuggestions([]);
+  };
   // Redirects to /map/city where city = input text or suggestion
   const submitHandler = (e) => {
     e.preventDefault();
-    setData(text)
+    setData(text);
     console.log(text);
-    console.log(e.target.city.value)
-    history.push(`/map/${e.target.city.value.replace(/ /g, '_')}`)
+    console.log(e.target.city.value);
+    history.push(`/map/${e.target.city.value.replace(/ /g, '_')}`);
   };
   // Button click handler
   const handleGoExplore = () => {
@@ -54,16 +53,28 @@ const Home = ({ history }) => {
     <div className='home'>
       <div className='home1'>
         <form onSubmit={submitHandler}>
-          <input type='text' name='city' onChange={(e) => onChangeHandler(e.target.value)} value={text} />
-          {
-            suggestions ? suggestions.map((suggestions, i) => <div key={i} onClick={() => onSuggestHandler(suggestions.name)}>{suggestions.name}</div>) : ""
-          }
+          <input
+            type='text'
+            name='city'
+            onChange={(e) => onChangeHandler(e.target.value)}
+            value={text}
+          />
+          {suggestions
+            ? suggestions.map((suggestions, i) => (
+                <div key={i} onClick={() => onSuggestHandler(suggestions.name)}>
+                  {suggestions.name}
+                </div>
+              ))
+            : ''}
           <p>Choose your next nomad destination according to your needs</p>
         </form>
         <button onClick={handleGoExplore}>Explore now</button>
       </div>
       <div className='home2'>
-        <p>Matching the needs of digital nomads and remote workers to the best place in Spain</p>
+        <p>
+          Matching the needs of digital nomads and remote workers to the best
+          place in Spain
+        </p>
         <ul>
           <li>Sunny places</li>
           <li>Safety</li>
@@ -76,21 +87,15 @@ const Home = ({ history }) => {
       <div className='home3'>
         <p>Best nomad places</p>
         <Slider
-          onSlideComplete={(i) => {
-            console.log('finished dragging, current slide is', i)
-          }}
-          onSlideStart={(i) => {
-            console.log('started dragging on slide', i)
-          }}
           activeIndex={0}
           threshHold={100}
           transition={0.5}
           scaleOnDrag={false}
         >
           {cities.map((city) => (
-            <div key={city.name} className="carousel__card">
+            <div key={city.name} className='carousel__card'>
               <img src={city.image} alt={city.name} />
-              <h2>{city.name}</h2>
+              <h3>{city.name}</h3>
             </div>
           ))}
         </Slider>
