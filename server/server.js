6678759934b072b45b6298b6e16caa8757f6dc41
IchikonWD <ezequielmigueles@icloud.com>
2021-10-29 05:02:43 +0200

    Last Try

--- server/server.js
+++ server/server.js
@@ -31,6 +31,7 @@ app.use('/api/upload', uploadRoutes);
 //path.join(__dirname, '../client/build/index.html')
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
+  app.use(express.static('/uploads'));
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
   });
@@ -41,10 +42,9 @@ if (process.env.NODE_ENV === 'development') {
     res.send('API is running...');
   });
   app.use(morgan('dev'));
+  const __dirname = path.resolve();
+  app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 }
-
-const __dirname = path.resolve();
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 app.use(notFound);
 app.use(errorHandler);
