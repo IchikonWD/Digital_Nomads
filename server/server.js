2ba189dd417d6ca645759b3ffec37b761a7074f0
Ezequiel Migueles Abraira <ezequielmigueles@icloud.com>
2021-10-29 03:48:20 +0200

    Hotfix

--- server/server.js
+++ server/server.js
@@ -31,6 +31,7 @@ app.use('/api/upload', uploadRoutes);
 //path.join(__dirname, '../client/build/index.html')
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('/client/build'));
+  app.use(express.static('public'));
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
   });
